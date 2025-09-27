7288608dc089622cdab38d997fd9ca753752aec4
rodrigo-sales <rodrigo.nasc2@gmail.com>
2025-09-27 12:20:03 -0300

    6. Classes TestLoader e TestRunner

--- test_classes.py
+++ test_classes.py
@@ -1,23 +1,18 @@
-from test_case_test import TestCaseTest
 from test_result import TestResult
-from test_suite import TestSuite
-from test_suite_test import TestSuiteTest
+from test_loader import TestLoader
+from test_loader_test import TestLoaderTest
+from test_runner import TestRunner
 
+# Testando com Teste Loader
 result = TestResult()
-suite = TestSuite()
-
-suite.add_test(TestCaseTest('test_result_success_run'))
-suite.add_test(TestCaseTest('test_result_failure_run'))
-suite.add_test(TestCaseTest('test_result_error_run'))
-suite.add_test(TestCaseTest('test_result_multiple_run'))
-suite.add_test(TestCaseTest('test_was_set_up'))
-suite.add_test(TestCaseTest('test_was_run'))
-suite.add_test(TestCaseTest('test_was_tear_down'))
-suite.add_test(TestCaseTest('test_template_method'))
-
-suite.add_test(TestSuiteTest('test_suite_size'))
-suite.add_test(TestSuiteTest('test_suite_success_run'))
-suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
+loader = TestLoader()
+suite = loader.make_suite(TestLoaderTest)
 suite.run(result)
 print(result.summary())
+
+# Testando com Teste Runner
+loader = TestLoader()
+suite = loader.make_suite(TestLoaderTest)
+
+runner = TestRunner()
+runner.run(suite)
