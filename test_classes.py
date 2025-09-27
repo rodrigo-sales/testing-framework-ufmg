9bfa52059c68029e44dcb8e263bd4832995d13b9
rodrigo-sales <rodrigo.nasc2@gmail.com>
2025-09-27 11:41:30 -0300

    3. Classe TestResult

---
+++ test_classes.py
@@ -0,0 +1,15 @@
+from test_case import MyTest
+from test_result import TestResult
+
+result = TestResult()
+
+test = MyTest('test_a')
+test.run(result)
+
+test = MyTest('test_b')
+test.run(result)
+
+test = MyTest('test_c')
+test.run(result)
+
+print(result.summary())
